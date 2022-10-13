fe628775d158e1bd5537749c6a170d5063898c42
Danial Nabil <danialnabil0208@gmail.com>
2022-10-13 15:01:13 +0800

    added splash, login,forgot password page

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  isLogin: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  backTologin(){
+    this.isLogin = true;
+  }
+
+  forgotPassword(){
+    this.isLogin = false;
+  }
+
+}
